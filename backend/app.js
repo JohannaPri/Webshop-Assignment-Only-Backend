275e32e3b5b10b31d22dfe20db9dc6790d6f33ee
Johanna Prinz <Johannaa.L@hotmail.com>
2024-02-01 14:41:48 +0100

    Add files and add endpoints in users.js

--- backend/app.js
+++ backend/app.js
@@ -5,8 +5,9 @@ const logger = require('morgan');
 const { MongoClient } = require('mongodb');
 const mongoClient = require('mongodb').MongoClient;
 
-// const indexRouter = require('./routes/index');
-// const usersRouter = require('./routes/users');
+const usersRouter = require('./routes/users');
+const productsRouter = require('./routes/products');
+const ordersRouter = require('./routes/orders');
 
 const app = express();
 
@@ -16,15 +17,20 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-mongoClient.connect("mongodb://127.0.0.1:27017")
+const MONGODB_URI = 'mongodb://127.0.0.1:27017';
+const DB_NAME = 'johanna-larssonprinz';
+
+mongoClient.connect(MONGODB_URI)
 .then(client => {
     console.log("The database is connected");
 
-    const db = client.db("johanna-larssonprinz");
+    const db = client.db(DB_NAME);
     app.locals.db = db;
+
+    app.use('/api/users', usersRouter);
+    app.use('/api/products', productsRouter);
+    app.use('/api/orders', ordersRouter);
 });
 
-// app.use('/', indexRouter);
-// app.use('/users', usersRouter);
-
 module.exports = app;
+
--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,9 +1,54 @@
 const express = require('express');
 const router = express.Router();
+const { ObjectId } = require('mongodb');
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.post('/add', async (req, res) => {
+  try {
+    const { name, email, password } = req.body;
+    const usersCollection = req.app.locals.db.collection('users');
+    const newUser = {  name, email, password };
+    const result = await usersCollection.insertOne(newUser);
+    res.status(201).json(result);
+  } catch (error) {
+    res.status(500).json({ error: error.message});
+  }
+});
+
+router.get('/', async (req, res) => {
+  try {
+    const usersCollection = req.app.locals.db.collection('users');
+    const users = await usersCollection.find({}, { projection: { password: 0 } }).toArray();
+    res.json(users);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+router.post('/', async (req, res) => {
+  try {
+    const userId = req.body.id;
+    const userIdObjectID = new ObjectId(userId);
+    const usersCollection = req.app.locals.db.collection('users');
+    const user = await usersCollection.findOne({ _id: userIdObjectID });
+    if (!user) {
+      res.status(404).json({ message: 'User not found'});
+      return;
+    }
+    res.json(user);
+   } catch (error) {
+    res.status(500).json({ error: error.message });
+   }
+});
+
+router.post('/login', async (req, res) => {
+  const { email, password } = req.body;
+  const usersCollection = req.app.locals.db.collection('users');
+  const user = await usersCollection.findOne({ email: email, password: password});
+  if (user) {
+    res.json({ message: 'Login successful', user});
+  } else {
+    res.status(401).json({ message: 'Invalid credentials' });
+  }
 });
 
 module.exports = router;
